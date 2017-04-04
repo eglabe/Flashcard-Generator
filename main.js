99188e42ffa16b32e2d4754ee36d221d96eded07
Erin <eringlabe@gmail.com>
2017-04-04 09:49:00 -0700

    added switch command

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 console.log("Instructions: " + 
-	"\nUse command 'add' to follow the prompts and add a new flashcard.");
+	"\nUse command 'add' to follow the prompts and add a new flashcard."
+	"\nUse command 'study' to study your flashcards."
+	"\nUse command 'review' to review your flashcards."
+
+);
 
 
 var command = process.argv[2];
@@ -8,9 +12,10 @@ switch(command) {
 	case add:
         addCard()
         break;
+    case study:
+        studyBasic()
+        break;        
     case review:
-        review()
+        reviewCloze()
         break;
-    default:
-        example()
 }
