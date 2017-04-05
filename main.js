65a1d3c9ca3eb1c175998d673c198efc9f7d64e3
Erin <eringlabe@gmail.com>
2017-04-05 12:36:34 -0700

    inquirer prompt and if statement for no command created

--- main.js
+++ main.js
@@ -1,26 +1,51 @@
-console.log("Instructions: "
-	+ "\nUse command 'add' to follow the prompts and add a new flashcard."
-	// + "\nUse command 'study' to study your flashcards."
-	// + "\nUse command 'review' to review your flashcards."
-);
-
+var inquirer = require("inquirer");
 
 var command = process.argv[2];
 
+if (!command) {
+	console.log("Instructions: "
+	+ "\nUse command 'add' to follow the prompts and add a new flashcard."
+	// + "\nUse command 'study' to study your flashcards."
+	// + "\nUse command 'review' to review your flashcards."
+	);
+}
+
 switch(command) {
-	case add:
+	case "add":
         addCard()
         break;
-    case study:
+    case "study":
         studyBasic()
         break;        
-    case review:
+    case "review":
         reviewCloze()
         break;
 }
 
 
-// write inquirer prompts
+function addCard() {
+	inquirer.prompt([
+		{
+		  name: "question",
+		  message: "Type in a study question. (front of flashcard)"
+		}, {
+		  name: "answer",
+		  message: "What is the anwer to the question you asked? (back of flashcard)"
+		}, {
+		  name: "full",
+		  message: "Turn the 'queston' and 'answer' above into a full sentence. (for review)"
+		}
+	]).then(function(input) {
+		console.log(input.question);
+		console.log(input.answer);
+		console.log(input.full);
+		// // initializes the variable newguy to be a programmer object which will take
+		// // in all of the user's answers to the questions above
+		// var newGuy = new Programmer(answers.name, answers.position, answers.age, answers.language);
+		// // printInfo method is run to show that the newguy object was successfully created and filled
+		// newGuy.printInfo();
+	});
+}
 
 // addCard function to initiate constructors
 
