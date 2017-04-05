692355edfab196400017d45196e2347ab11dd12a
Erin <eringlabe@gmail.com>
2017-04-05 14:18:06 -0700

    attempts at creating function for partial in clozecard

--- ClozeCard.js
+++ ClozeCard.js
@@ -3,7 +3,16 @@ function ClozeCard (full, answer) {
 	this.cloze = answer;
 
 	this.partial = function() {
+		var fullArr = this.full.split(" ");
+		console.log(fullArr);  // 'George', 'Washington', 'was', 'the', 'first', 'president'
+		var clozeArr = this.cloze.split(" ");
+		console.log(clozeArr);  // 'George', 'Washington'
 
+
+
+		// var index = this.full.indexOf("George");
+		// console.log(index);
+		// console.log(this.full[index]);
 	}
 
 	this.reviewCloze = function() {
@@ -12,3 +21,7 @@ function ClozeCard (full, answer) {
 }
 
 module.exports = ClozeCard;
+
+var first = new ClozeCard("George Washington was the first president", "George Washington");
+console.log(first.full);
+first.partial();
